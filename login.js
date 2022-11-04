106cdd70b5af47837ecdac614ad46787f63ffa59
dkrnjic <dkrnjic@student.unipu.hr>
2022-11-04 20:52:41 +0100

    Male izmjene na loginu

--- login.js
+++ login.js
@@ -1,15 +1,24 @@
 
-//test btn
-const testBtn = document.getElementById('testBtn');
+const toggleForm1 = document.getElementsByClassName('toggleForm')[0];
+const toggleForm2 = document.getElementsByClassName('toggleForm')[1];
 let container1 = document.getElementsByClassName('container')[0];
 let container2 = document.getElementsByClassName('container')[1];
+let forma1= document.getElementsByClassName('forms')[0];
+let forma2= document.getElementsByClassName('forms')[1];
 
-testBtn.addEventListener('click' ,() => {
+toggleForm1.addEventListener('click' ,() => {
     container1.classList.toggle('active')
     container2.classList.toggle('active')
-    console.log("radi");
+    forma1.reset();   
+    forma2.reset();   
 })
 
+toggleForm2.addEventListener('click' ,() => {
+    container1.classList.toggle('active')
+    container2.classList.toggle('active')
+    forma1.reset();   
+    forma2.reset();   
+})
 
 //toggleSwitch
 let btn = document.getElementById('btn');
